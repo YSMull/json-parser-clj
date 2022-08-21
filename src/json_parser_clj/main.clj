001129d986158d5241dfd2d628331648e7a2fee9
毛煜苏 <maoyusu@corp.netease.com>
2022-08-21 13:14:31 +0800

    bench

--- src/json_parser_clj/main.clj
+++ src/json_parser_clj/main.clj
@@ -1,4 +1,5 @@
 (ns json-parser-clj.main
+  (:gen-class)
   (:require [json-parser-clj.lexer-ref-version :as ref-version-lexer])
   (:use [json-parser-clj.lexer]
         [json-parser-clj.parser]
@@ -6,21 +7,64 @@
 
 (set! *warn-on-reflection* true)
 
-(defn -main
-  []
-  (let [json-str (slurp "./test.json")]
-    (do
-      (let [[tokens] (tokenize json-str)
-            ast (parse tokens)] (do
-                                  (println (json-minify ast))
-                                  (println (json-stringify ast 2))))
 
-      (let [tokens (ref-version-lexer/tokenize json-str)
-            ast (parse tokens)] (do
-                                  (println (json-minify ast))
-                                  (println (json-stringify ast 2))))
+;(let [json-str (slurp json-path)]
+;  (do
+;    (let [[tokens] (tokenize json-str)
+;          ast (parse tokens)] (do
+;                                (println (json-minify ast))
+;                                (println (json-stringify ast 2))))
+;
+;    (let [tokens (ref-version-lexer/tokenize json-str)
+;          ast (parse tokens)] (do
+;                                (println (json-minify ast))
+;                                (println (json-stringify ast 2))))
+;
+;    (time (dotimes [_ 100] (tokenize json-str)))
+;    (time (dotimes [_ 100] (ref-version-lexer/tokenize json-str)))
+;    ))
 
-      (time (dotimes [_ 1000] (tokenize json-str)))
-      (time (dotimes [_ 1000] (ref-version-lexer/tokenize json-str)))
-      ))
+(defn json-parse [^String json-path]
+  (let [json-str (slurp json-path)]
+    (let [[tokens] (tokenize json-str)] (parse tokens))))
+
+(defn json-stringify [^String ast]
+  (json-parser-clj.util/stringify ast))
+
+(defn json-prettify [^String ast]
+  (json-parser-clj.util/prettify ast 2))
+
+
+(def check-directory (clojure.java.io/file "./data/jsonchecker"))
+
+(defn validate []
+  (let [json-files (.listFiles check-directory)
+        check-json (fn [file] (let [path (str file)
+                                    _ (try
+                                        (do (json-parse path)
+                                            (println (str path " passed!")))
+                                        (catch Exception e
+                                          (println (str path " failed!"))))
+                                    ] nil))]
+    (mapv check-json json-files)))
+
+(def test-files ["./data/twitter.json" "./data/citm_catalog.json" "./data/canada.json"])
+
+(defn -main []
+  (do
+    ;(validate)
+    ; warmup
+    (time (dotimes [_ 10] (let [asts (mapv json-parse test-files)
+                               _ (mapv json-stringify asts)
+                               ;_ (mapv json-prettify asts)
+                               ] nil)))
+    (println)
+    ; test
+    (dotimes [_ 3] (do
+                     (let [asts (time (mapv json-parse test-files))
+                           _ (time (mapv json-stringify asts))
+                           _ (time (mapv json-prettify asts))
+                           ] nil)
+                     (println)))
+    )
   )
--- src/json_parser_clj/util.clj
+++ src/json_parser_clj/util.clj
@@ -1,17 +1,17 @@
 (ns json-parser-clj.util)
 
-(defn json-minify [ast]
+(defn stringify [ast]
   (case (first ast)
     :j-number (second ast)
     :j-string (str \" (second ast) \")
     :j-bool (second ast)
     :j-null "null"
     :j-array (let [elements (second ast)]
-               (str \[ (clojure.string/join "," (map json-minify elements)) \]))
+               (str \[ (clojure.string/join "," (map stringify elements)) \]))
     :j-object (let [kvs (second ast)]
-                (str \{ (clojure.string/join "," (map (fn [[k v]] (str \" k \" \: (json-minify v))) kvs)) \}))))
+                (str \{ (clojure.string/join "," (map (fn [[k v]] (str \" k \" \: (stringify v))) kvs)) \}))))
 
-(defn json-stringify [ast indent-width]
+(defn prettify [ast indent-width]
   (let [gen-ident (fn [width depth] (apply str (repeat (* depth width) " ")))
         helper (fn helper [depth force-indent? ast]
                  (cond->> (case (first ast)
